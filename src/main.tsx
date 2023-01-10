484d76977482b6c1b9fcc924f7260da7b2bace16
kiki-do <74352957+kiki-do@users.noreply.github.com>
2023-01-10 14:01:50 +0300

    Добавлен LocalStorage

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,7 @@ import { store } from "./store/store";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			{/* <PersistGate loading={null} persistor={persistor}> */}
 			<App />
-			{/* </PersistGate> */}
 		</Provider>
 	</React.StrictMode>
 );

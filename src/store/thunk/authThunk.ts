dee4bbec9526da978daf4768d9caada5dd88fd25
kiki-do <74352957+kiki-do@users.noreply.github.com>
2023-01-12 12:56:26 +0300

    Adding dist

--- src/store/thunk/authThunk.ts
+++ src/store/thunk/authThunk.ts
@@ -2,9 +2,9 @@ import { AuthItems } from "./../authSlice/types";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const typiCode = "https://my-json-server.typicode.com/kiki-do/authapi/auth";
+
 export const fetchAuth = createAsyncThunk("todo/fetchAuth", async () => {
-	const responce = await axios.get(
-		"https://my-json-server.typicode.com/kiki-do/authapi/auth"
-	);
+	const responce = await axios.get(typiCode);
 	return responce.data;
 });
--- src/store/thunk/itemsThunk.ts
+++ src/store/thunk/itemsThunk.ts
@@ -2,12 +2,13 @@ import { ItemsType } from "./../itemsSlice/types";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const mockAPI = "https://63b8b9ad6f4d5660c6dd8d43.mockapi.io/task";
+const typiCode = "https://my-json-server.typicode.com/kiki-do/authapi/contacts";
+
 export const fetchItems = createAsyncThunk<ItemsType[]>(
 	"todo/fetchItems",
 	async () => {
-		const responce = await axios.get(
-			"https://my-json-server.typicode.com/kiki-do/authapi/contacts"
-		);
+		const responce = await axios.get(typiCode);
 		return responce.data;
 	}
 );

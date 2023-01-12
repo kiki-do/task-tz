a64a7511bedbc200c1146f5071b88ea713d2e42f
kiki-do <74352957+kiki-do@users.noreply.github.com>
2023-01-12 18:25:44 +0300

    done

--- src/store/thunk/itemsThunk.ts
+++ src/store/thunk/itemsThunk.ts
@@ -2,8 +2,7 @@ import { ItemsType } from "./../itemsSlice/types";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const mockAPI =
-	"https://63b8b9ad6f4d5660c6dd8d43.mockapi.io/taskhttps://my-json-server.typicode.com/kiki-do/authapi/db";
+const mockAPI = "https://63b8b9ad6f4d5660c6dd8d43.mockapi.io/task";
 const typiCode = "https://my-json-server.typicode.com/kiki-do/authapi/db";
 
 export const fetchItems = createAsyncThunk<ItemsType[]>(

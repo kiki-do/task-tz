c04743756a1eac72adf81951e199cba5013c3072
kiki-do <74352957+kiki-do@users.noreply.github.com>
2023-01-12 18:54:27 +0300

    Adding dist

--- src/store/thunk/authThunk.ts
+++ src/store/thunk/authThunk.ts
@@ -3,6 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const typiCode = "https://my-json-server.typicode.com/kiki-do/authapi/auth";
+
 export const fetchAuth = createAsyncThunk("todo/fetchAuth", async () => {
 	const responce = await axios.get(typiCode);
 	return responce.data;

136d066086c010eff852555a4cda65ff651e335e
kiki-do <74352957+kiki-do@users.noreply.github.com>
2023-01-12 18:29:55 +0300

    done

--- src/store/thunk/authThunk.ts
+++ src/store/thunk/authThunk.ts
@@ -2,7 +2,7 @@ import { AuthItems } from "./../authSlice/types";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const typiCode = "https://my-json-server.typicode.com/kiki-do/authapi/auth";
+const typiCode = "https://my-json-server.typicode.com/kiki-do/loginapi/db";
 export const fetchAuth = createAsyncThunk("todo/fetchAuth", async () => {
 	const responce = await axios.get(typiCode);
 	return responce.data;
